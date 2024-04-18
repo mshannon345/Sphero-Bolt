67438f9bf96d4b95f74e5c1c655bdc43477ee7ad
Shannon <milesshannon151@gmail.com>
2024-04-18 14:16:30 -0400

    Added Hello World script from Wiki

--- Sandbox.js
+++ Sandbox.js
@@ -1,10 +1,15 @@
 //Programmer: Miles Shannon
 //Team Member: Billy Felton
-//Bolt ID:
+//Bolt ID:SB-52F9
 //Date: 4.16.24
 //Program: Hello World - Sandbox
 //Program URL:https://edu.sphero.com/program/16700410/edit
 
 async function startProgram() {
-	// Write code here
+	await speak("Hello World", true);
+	setMainLed({ r: 0, g: 0, b: 255});
+	setSpeed(60);
+	await delay (2);
+	setSpeed(0);
+
 }
