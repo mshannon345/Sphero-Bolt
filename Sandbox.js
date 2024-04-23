f4666fb3fee589bd6bbef191d5d8c43916d8d9f9
Shannon <milesshannon151@gmail.com>
2024-04-23 13:21:09 -0400

    Changed Hello Square

--- Sandbox.js
+++ Sandbox.js
@@ -7,20 +7,13 @@
 
 async function startProgram() {
 
-	setMainLed({ r: 0, g: 0, b: 255 });
-
+	setMainLed({ r: 5, g: 24, b: 236 });
 	await speak("Hello Square", true);
-
 	await delay(1);
-
-	for (var _i1 = 0; _i1 < 4; _i1++) {
-
+	for (var helloSquare = 0; helloSquare < 4; helloSquare++) {
 		setMainLed(getRandomColor());
-
 		await Sound.Game.Coin.play(true);
-
 		await roll((getHeading() + 90), 60, 1);
-
 		await delay(1);
 
 }
