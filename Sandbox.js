026927c534b9e60fd4f64d254970af2406369f70
Shannon <milesshannon151@gmail.com>
2024-04-23 13:22:10 -0400

    Changed Sound

--- Sandbox.js
+++ Sandbox.js
@@ -12,7 +12,7 @@ async function startProgram() {
 	await delay(1);
 	for (var helloSquare = 0; helloSquare < 4; helloSquare++) {
 		setMainLed(getRandomColor());
-		await Sound.Game.Coin.play(true);
+		await Sound.Effects.Laser.play(true);
 		await roll((getHeading() + 90), 60, 1);
 		await delay(1);
 
