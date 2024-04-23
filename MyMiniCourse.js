136086ab62ffbb88204da78e33448618eef879b3
Shannon <milesshannon151@gmail.com>
2024-04-23 14:15:04 -0400

    Checkpoint 2 My Mini Course - stable

--- MyMiniCourse.js
+++ MyMiniCourse.js
@@ -5,7 +5,17 @@
 //Program: My Mini Course
 //Program URL:https://edu.sphero.com/program/16743355/edit
 
+
 // #1 Checkpoint 
 async function startProgram() {
 	await roll ( 0, 65, 2)
-} 
+	await delay(1);
+	
+// #2 Checkpoint 	
+	await roll ( 90, 65, 2)
+	await delay(1);
+
+
+
+}
+
