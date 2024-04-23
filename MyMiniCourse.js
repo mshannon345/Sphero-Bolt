0339b68ed513e58fd32f7d8499661f1a7bfadae8
Shannon <milesshannon151@gmail.com>
2024-04-23 13:36:12 -0400

    Initial Mini Course commit

---
+++ MyMiniCourse.js
@@ -0,0 +1,10 @@
+//Programmer: Miles Shannon
+//Team Member: Billy Felton
+////Bolt ID:SB-52F9
+//Date: 4.23.24
+//Program: My Mini Course
+//Program URL:https://edu.sphero.com/program/16743355/edit
+
+async function startProgram() {
+	// Write code here
+}
