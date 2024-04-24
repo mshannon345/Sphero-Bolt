b6ec6541fffce5fca52195374c99a128ad2d1c72
Shannon <milesshannon151@gmail.com>
2024-04-24 13:59:02 -0400

    ObstacleCourse 1st checkpoint Billy Felton

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -4,3 +4,9 @@
 //Date: 4.24.24
 //Program: My Mini Course
 //Program URL:https://edu.sphero.com/program/16752495/edit
+
+//Billy Felton Checkpoint 1
+async function startProgram() {
+	await roll( 80, 80, 2),
+	await delay(1);
+}
