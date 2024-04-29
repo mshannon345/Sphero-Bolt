f8e0fbc864a8f93e4ca4101a46dbd2b92c23a835
Shannon <milesshannon151@gmail.com>
2024-04-29 13:11:35 -0400

    Obstacle course Billy Felton - PC#30 - checkpoint 3 - Purple

--- ObstacleCourse.js
+++ ObstacleCourse.js
@@ -13,4 +13,9 @@ async function startProgram() {
 //Miles Shannon Checkpoint 2
 	await roll(-25, 94 , 2),
 	await delay(1);
+
+//Billy Felton - PC#30 - Checkpoint 3 - Purple
+	setMainLed({ r: 255, g: 0, b: 255 }),
+		await roll (-152, 83, 2),
+		await delay(1);
 }
