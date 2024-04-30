889dfc4fb80ecad091268132b61c985a4c9fdc39
Shannon <milesshannon151@gmail.com>
2024-04-30 14:12:51 -0400

    Billy Felton- Start text & Audion Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -6,5 +6,37 @@
 //Program URL:https://edu.sphero.com/program/16788324/edit
 
 async function startProgram() {
-	// Write code here
+	//Programmer: Billy Felton - Start Text & Audio Chackpoint Commit - Date: 4.30.24 
+		await scrollMatrixText('Start!', { r: 66, g: 56, b: 255 }, 15, true)
+		await speak("Start", true),
+		
+
+
+	//Programmer: - Blue LED Checkpoint Date:
+
+
+
+	//Programmer: - Sound One Checkpoint Commit - Date:
+
+
+
+	//Programmer: -Red LED Checkpoint Date:
+
+
+
+	//Programmer: -Sound Two Checkpoint Commit - Date:
+
+
+
+	//Programmer: -Green LED Checkpoint Date:
+
+
+
+	//Programmer: -Purple LED & Sound Three Checkpoint Date:
+
+
+
+	//Programmer: -Text and Audio Commit - Date
+
+
 }
