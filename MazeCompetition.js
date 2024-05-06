d341561c318260a4b032cb0eb9395dae5005ecba
Shannon <milesshannon151@gmail.com>
2024-05-06 14:20:13 -0400

    5.6.2024 Commit

--- MazeCompetition.js
+++ MazeCompetition.js
@@ -12,14 +12,14 @@ async function startProgram() {
 
 
 	// Programmer: Miles Shannon - Blue LED Checkpoint Commit - Date: 4.30.24
-			await roll (0, 72, 2),
+			await roll (0, 74, 2),
 			await delay(1),
 		setMainLed({ r: 0, g: 0, b: 255 });
 
 
 
 	// Programmer: Billy Felton - Sound One Checkpoint Commit - Date:5.1.24
-			await roll (90, 51, 2),
+			await roll (90, 55, 2),
 			await delay(1),
 			await Sound.Effects.Laser.play(true);
 
@@ -34,21 +34,29 @@ async function startProgram() {
 
 
 	// Programmer: Billy Felton - Sound Two Checkpoint Commit - Date: 5.2.24
-			await roll((getHeading() - 90), 21, 2),
+			await roll((getHeading() - 90), 23, 2),
 			await delay(1),
-			await roll((getHeading() - 85), 40, 2),
+			await roll((getHeading() - 90), 43, 2),
 			await delay(1),
 			await roll((getHeading() + 85), 23, 2),
 			await Sound.Effects.Explosion.play(true);
 	
-	// Programmer: Miles Shannon - Green LED Checkpoint Commit - Date: 5.2.24
-			await roll((getHeading() + 62) , 28, 2),
+	// Programmer:  Miles Shann- Green LED Checkpoint Commit - Date: 4.2.24
+			await roll((getHeading() + 90) , 28, 2),
 			await delay(1),
 			setMainLed({ r: 0, g: 255, b: 0 });
 
 
 
-	// Programmer:  - Purple LED & Sound Three Checkpoint Commit - Date:
+	// Programmer: Billy Felton - Purple LED & Sound Three Checkpoint Commit - Date:
+			await roll((getHeading() - 90) , 26, 2),
+			await delay(1),
+			await roll((getHeading() - 90) , 40, 2),
+			await delay(1),
+			await roll((getHeading() - 40) , 23, 2),
+			await delay(1),
+			setMainLed({ r: 255, g: 0, b: 255 }),
+			await Sound.Effects.Applause.play(true);
 
 
 
